8db02258e2f1eb702c76a3b705b9e644e560ef70
Unique JJ <49803637+unique-jj@users.noreply.github.com>
2024-07-25 17:06:27 +0900

    Add files via upload

---
+++ 1.variable/6.empty.js
@@ -0,0 +1,14 @@
+//null, undefined
+let variable;
+console.log(variable);
+
+variable = null;
+console.log(variable);
+
+let activeItem; // 아직 활성화된 아이템이 있는지 없는지 모르는 상태! undefined
+activeItem = null; // 활성화된 아이템이 없는 상태!
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(null === undefined); //false
+console.log(typeof "123"); //string
---
+++ 1.variable/7.object.js
@@ -0,0 +1,18 @@
+let name = "apple";
+let color = "red";
+let orangeName = "orange";
+
+let apple = {
+  name: "apple",
+  color: "red",
+};
+console.log(apple); // { name: 'apple', color: 'red' }
+console.log(apple.name); // apple
+console.log(apple.color); // red
+
+let orange = {
+  name: "오렌지",
+  color: "orange",
+};
+
+console.log(orange); // { name: '오렌지', color: 'orange' }
